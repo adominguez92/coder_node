01ebc235a03efaf6a85fbaa467182c76791ed925
Agustin Dominguez <agndoz@gmail.com>
2022-10-27 13:18:26 -0300

    cambio de formato y modificaciones para entrega

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const Contenedor = require('./Contenedor.js')
+
+const app = express()
+const port = process.env.PORT|| 8080
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo estamos online!')
+})
+
+app.get('/productos', async (req, res) => {
+  
+  const contenedor = new Contenedor();
+  const todos = await contenedor.getAll();
+  res.json(todos)
+})
+
+app.get('/productoRandom', async (req, res) => {
+  const generarAleatorio = (min, max) => {
+    return Math.floor(Math.random() * (1 + max - min)) + min;
+  }
+  const productoID = generarAleatorio(1,3).toString();
+  const contenedor = new Contenedor();
+  const producto = await contenedor.getById(productoID)
+  res.json(producto)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- test.js
+++ test.js
@@ -4,20 +4,20 @@ const testApp = async () => {
 
     const contenedor = new Contenedor()
     
-     await contenedor.save({    
+    /* await contenedor.save({    
         "title":"Peluche de Picachu",
         "price":"1600",
         "thumbnail":"https://res.cloudinary.com/none-img/image/upload/v1663341518/Peluches/pika_mfzfuu.jpg",
         "id":""
     }) 
-    
-    console.log(await contenedor.getAll());
+    */
+   // console.log(await contenedor.getAll());
     
     console.log(await contenedor.getById('3'));
-    
+    /*
     await contenedor.deleteById('1');
     
-    await contenedor.deleteAll();    
+    await contenedor.deleteAll();    */
     }
     
     testApp()
