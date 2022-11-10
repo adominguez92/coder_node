c67166b2209c7327f1bc3fd9333d8247d6bfd4e1
Agustin Dominguez <agndoz@gmail.com>
2022-11-10 13:44:28 -0300

    Modificaciones para entraga

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
 const express = require('express')
+const { render } = require('pug')
 const { Router } = express
 const Contenedor = require('./Contenedor.js')
+//const { engine } = require('express-handlebars'); //cuando uso express-handlebars habilitar
 
 const app = express()
 const routerProd = Router()
 const port = process.env.PORT|| 8080
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(__dirname + '/public'));
+/*app.set('view engine', 'hbs');//configuracion para express-handlebars
+app.set('views', './views');
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+  })
+);*/
 
+/*app.set('view engine', 'pug'); // configuracion para pug
+app.set('views', './views');*/
+
+app.set('view engine', 'ejs'); //configuracion para ejs
+
+/**
+  API
+**/
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on http://localhost:${port}`)
 })
 app.use('/api/productos', routerProd);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Servidor AD </h1>');
+  res.send('<h1>Servidor AD</h1>');
 });
 
 app.get('/formulario', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
 
 routerProd.get('/', async (req, res) => {
   const contenedor = new Contenedor();
@@ -47,13 +68,44 @@ routerProd.put('/:id', async (req, res)=>{
   const { id } = req.params
   const { body } = req
   const contenedor = new Contenedor()
-  let resultado = contenedor.modfByID(id.toString(), body.title, body.price, body.thumbnail)
+  let resultado = await contenedor.modfByID(id.toString(), body.title, body.price, body.thumbnail)
   res.json(resultado)
 })
 
 routerProd.delete('/:id', async (req, res)=>{
   const { id } = req.params
   const contenedor = new Contenedor()
-  let resultado = contenedor.deleteById(id.toString())
+  let resultado = await contenedor.deleteById(id.toString())
   res.json(resultado)
 })
+
+/**
+  Render de Plantillas
+**/
+
+app.get('/products', async (req, res)=>{
+  const contenedor = new Contenedor();
+  const todos = await contenedor.getAll();
+  res.render('pages/products', {title: 'Lista de productos', products: todos })
+})
+
+
+app.post('/products', async (req, res)=>{
+  const { body } = req
+  const contenedor = new Contenedor()
+  const prodAgregado = await contenedor.save(body)
+  res.json(prodAgregado)
+  res.render('pages/ingresado')
+})
+
+app.get('/form', (req, res)=>{
+  res.render('pages/form', {title:'Ingresar un producto'})
+})
+
+
+// usando handlebars
+/*app.get('/products', async (req, res) => {
+  const contenedor = new Contenedor();
+  const todos = await contenedor.getAll();
+  res.render('productslist', {productsExist: true, products: todos})
+})*/
